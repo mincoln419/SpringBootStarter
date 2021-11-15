b2f0007fdfd043354d2b74eb450fde32bc98395b
mincoln <mincoln419@naver>
2021-11-15 18:28:02 +0900

    Holoman

---
+++ src/main/java/com/main/HolomanRunner.java
@@ -0,0 +1,21 @@
+package com.main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import com.configure.Holoman;
+
+@Component
+public class HolomanRunner implements ApplicationRunner{
+
+	@Autowired
+	Holoman holoman;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println(holoman.toString());
+		
+	}
+}
